ef7825d108d4bc51750b2048760ca0fa726ec253
Kibet <mshckkibet@gmail.com>
2024-03-08 14:39:17 +0300

    played with pages

--- products/models.py
+++ products/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title=models.CharField(max_length=120)
-    description=models.TextField(blank=True, null=True)
+    description=models.TextField(blank=False, null=True)
     price=models.DecimalField(decimal_places=2,max_digits=1000)
     summary=models.TextField(default='This is so cool')
+    featured=models.BooleanField()
+    data=models.TextField(blank=True,null=False)
