7e0ac1e7cfd30517e28ea29fa2121e5d41a94d0f
Kibet <mshckkibet@gmail.com>
2024-03-10 14:43:55 +0300

    added contexts

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse('<h1>Welcome back</h1>')
+def home_view(request,*args, **kwargs):
+    context ={
+        'banana':23,
+        'lemon':[21,32,4,43,54,5,65]
+    }
+    return render(request,'home.html', context)
+
+def about_view(request,*args, **kwargs):
+    return render(request, 'about.html',{})
