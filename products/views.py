b53c7f59eba184b8fc4caa9a5bd15a1c3e5d9efd
Kibet <mshckkibet@gmail.com>
2024-03-10 15:44:19 +0300

    view models from template

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    context = {
+        'object':obj
+        # 'title':obj.title,
+        # 'd':obj.description,
+        # 'price':obj.price
+    }
+    return render(request, 'product/detail.html',context)
+    
