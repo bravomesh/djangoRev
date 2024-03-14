bf2ace3411770bb0265876705f17455586c181f7
Kibet <mshckkibet@gmail.com>
2024-03-14 15:51:22 +0300

    finalized

--- firtPrj/urls.py
+++ firtPrj/urls.py
@@ -1,28 +1,45 @@
-"""
-URL configuration for firtPrj project.
+# """
+# URL configuration for firtPrj project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/5.0/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
+# from django.contrib import admin
+# from django.urls import path
+# from pages.views import home_view,about_view
+# from .views import (product_detail_view, product_create_view,product_delete_view, product_list_view, product_update_view
+# )
+# app_name = 'products'
+# urlpatterns = [
+#     path('', product_list_view, name='product-list'),
+#     path('create/', product_create_view, name='product-list'),
+#     path('<int:id>/', product_detail_view, name='product-detail'),
+#     path('<int:id>/update/', product_update_view, name='product-update'),
+#     path('<int:id>/delete/', product_delete_view, name='product-delete'),
+# ]  
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
 from pages.views import home_view,about_view
-from products.views import product_detail_view, product_create_view
+
 
 urlpatterns = [
+    path('products/', include('products.urls')),
+    path('', home_view, name='home'),
+    path('about/<int:id>/', about_view, name='product-detail'),
+
     path('admin/', admin.site.urls),
-    path('', home_view,name='home'),
-    path('about/', about_view),
-    path('product/<int:my_id>/', product_detail_view,name='product-detail'),
-    path('create/', product_create_view)
 ]
+
--- products/views.py
+++ products/views.py
@@ -1,41 +1,93 @@
-from django.shortcuts import render
-from django.http import Http404
-from .forms import ProductForm, RawProductForm
-from .models import Product
-# Create your views here.
-def product_detail_view(request, my_id):
-    try:
-        obj = Product.objects.get(id=my_id)
-    except Product.DoesNotExist:
-        raise Http404('Product does not exist')
-    context = {
-        'object':obj 
-        # 'title':obj.title,
-        # 'd':obj.description,
-        # 'price':obj.price
-    }
-    return render(request, 'products/product_detail.html',context)
+# from django.shortcuts import render
+# from django.http import Http404
+# from .forms import ProductForm, RawProductForm
+# from .models import Product
+# # Create your views here.
+# def product_detail_view(request, my_id):
+#     try:
+#         obj = Product.objects.get(id=my_id)
+#     except Product.DoesNotExist:
+#         raise Http404('Product does not exist')
+#     context = {
+#         'object':obj 
+#         # 'title':obj.title,
+#         # 'd':obj.description,
+#         # 'price':obj.price
+#     }
+#     return render(request, 'products/product_detail.html',context)
+
+# # def product_create_view(request):
+# #     form = ProductForm(request.POST or None)
+# #     if form.is_valid():
+# #         form.save()
+# #         form=ProductForm()
+        
+# #     context={
+# #         'form': form
+# #     }
+    
+# #     return render(request, 'products/product_create.html', context)
 
 # def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form=ProductForm()
-        
-#     context={
+#     initial_data={
+#         'title':'this awesome title',
+#         'description':'this awesome description is okayg'
+#     }
+#     form = ProductForm(request.POST or None, initial = initial_data)
+    
+#     context={ 
 #         'form': form
 #     }
-    
 #     return render(request, 'products/product_create.html', context)
 
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import ProductForm
+from .models import Product
+
+
 def product_create_view(request):
-    initial_data={
-        'title':'this awesome title',
-        'description':'this awesome description is okayg'
-    }
-    form = ProductForm(request.POST or None, initial = initial_data)
-    
-    context={ 
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+    context = {
         'form': form
     }
-    return render(request, 'products/product_create.html', context)
+    return render(request, "products/product_create.html", context)
+
+
+def product_update_view(request, id=id):
+    obj = get_object_or_404(Product, id=id)
+    form = ProductForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
+
+
+def product_list_view(request):
+    queryset = Product.objects.all() # list of objects
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "products/product_list.html", context)
+
+def product_detail_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    context = {
+        "object": obj
+    }
+    return render(request, "products/product_detail.html", context)
+
+
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    if request.method == "POST":
+        obj.delete()
+        return redirect('../../')
+    context = {
+        "object": obj
+    }
+    return render(request, "products/product_delete.html", context)
